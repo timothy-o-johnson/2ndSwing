ff2301185e882ac1739ecdccaf9776894d1f6488
Timothy Johnson <tjohnson@2ndswing.com>
2020-03-02 09:03:51 -0500

    periodic backup

---
+++ NG-1838-error-in-facility-to-vendor-overview.js
@@ -0,0 +1,17 @@
+var details = {
+    link: 'https://2ndswing.atlassian.net/browse/NG-1838',
+    errorMessage: 'Usage Limit Exceeded',
+    file: 'suiteScripts/UE_Vendor_Set_Order_Facility.js',
+    function: 'aferSubmit()',
+    row: 38,
+    action: `
+        1. create MR Script
+            - add two params (poIds, newFacilityId)
+            
+    
+    `
+    
+    
+        
+    }
+}
--- debugger-template.js
+++ debugger-template.js
@@ -6,12 +6,151 @@ require([
   'N/search',
   'N/record',
   '/SuiteScripts/WMS/shared/SavedSearchLibrary',
-  'SuiteScripts/LIB_SearchHelpers', '/SuiteScripts/WMS/shared/ItemHelper'
-
+  'SuiteScripts/LIB_SearchHelpers',
+  '/SuiteScripts/WMS/shared/ItemHelper'
 ], function (search, record, ssLib, searchHelpers, itemHelper) {
-  // enter functions here
-  //
-  //
+  // ADD CODE BELOW
+  // ADD CODE BELOW
+  // ADD CODE BELOW
+  // ADD CODE ABOVE
+  // ADD CODE ABOVE
+  // ADD CODE ABOVE
+})
+
+// NG-1842- getPreferredVendor()
+require([
+  'N/search',
+  'N/record',
+  '/SuiteScripts/WMS/shared/SavedSearchLibrary',
+  'SuiteScripts/LIB_SearchHelpers',
+  '/SuiteScripts/WMS/shared/ItemHelper'
+], function (search, record, ssLib, searchHelpers, itemHelper) {
+  // ADD CODE BELOW
+  // ADD CODE BELOW
+  // ADD CODE BELOW
+
+  var childItem = 3009123
+
+  var thisItem = record.load({
+    type: record.Type.INVENTORY_ITEM,
+    id: childItem
+  })
+
+  var brandId = thisItem.getValue({ fieldId: 'custitem_wms_parentitembrand' })
+
+  log.debug(getPreferredVendorId(brandId))
+
+  function getPreferredVendorId (brandId) {
+    var preferredVendorId
+
+    try {
+      var preferredVendorIdObj = search.lookupFields({
+        type: 'customrecord_g2_brand',
+        id: brandId,
+        columns: ['custrecord_preferred_vendor']
+      })
+
+      log.debug('preferredVendorId: raw', preferredVendorId)
+
+      preferredVendorId =
+        preferredVendorIdObj['custrecord_preferred_vendor'][0].value
+    } catch (e) {
+      // if there was no match based on the brand in the online title, then return id for Vendor: Ping
+
+      preferredVendorId = 1290699
+
+      log.error(
+        'no vendor found on parent item, using default vendor:' +
+          preferredVendorId,
+        e
+      )
+    }
+    return preferredVendorId
+  }
+
+  // ADD CODE ABOVE
+  // ADD CODE ABOVE
+  // ADD CODE ABOVE
+})
+
+// NG-1842
+require([
+  'N/search',
+  'N/record',
+  '/SuiteScripts/WMS/shared/SavedSearchLibrary',
+  'SuiteScripts/LIB_SearchHelpers',
+  '/SuiteScripts/WMS/shared/ItemHelper'
+], function (search, record, ssLib, searchHelpers, itemHelper) {
+  // ADD CODE BELOW
+  // ADD CODE BELOW
+  // ADD CODE BELOW
+
+  var parentItems = [45515]
+  var vendor = 152855
+
+  setVendorOnParentItems(parentItems, vendor)
+
+  function setVendorOnParentItems (parentItems, vendor) {
+    var updatedRecords = []
+    var parentItemRecord, confirmationId, newVendor, oldVendor //updatedRecord
+
+    parentItems.forEach(function (parentId) {
+      parentItemRecord = record.load({
+        type: 'inventoryitem',
+        id: parentId
+      })
+
+      oldVendor = parentItemRecord.getValue({
+        fieldId: 'vendor'
+      })
+
+      oldVendorText = parentItemRecord.getText({
+        fieldId: 'vendor'
+      })
+
+      log.debug(
+        'item # / oldVendor / oldVendorText',
+        parentId + '/ ' + oldVendor + '/ ' + oldVendorText
+      )
+
+      parentItemRecord.setValue({
+        fieldId: 'vendor',
+        value: vendor,
+        ignoreFieldChange: true
+      })
+
+      parentItemRecord.setText({
+        fieldId: 'vendor',
+        value: vendor,
+        ignoreFieldChange: true
+      })
+
+      confirmationId = parentItemRecord.save()
+      // updatedRecord = record.submitFields({
+      //   type: 'inventoryitem',
+      //   id: parent,
+      //   values: { vendor: vendor }
+      // })
+      parentItemRecord = record.load({
+        type: 'inventoryitem',
+        id: parentId
+      })
+
+      newVendor = parentItemRecord.getValue({
+        fieldId: 'vendor'
+      })
+
+      log.debug('item # / newVendor', parentId + '/ ' + newVendor)
+
+      updatedRecords.push(confirmationId)
+    })
+
+    return updatedRecords
+  }
+
+  // ADD CODE ABOVE
+  // ADD CODE ABOVE
+  // ADD CODE ABOVE
 })
 
 // NG-1568
@@ -23,17 +162,18 @@ require([
   'SuiteScripts/LIB_SearchHelpers',
   '/SuiteScripts/WMS/shared/ItemHelper'
 ], function (search, record, ssLib, searchHelpers, itemHelper) {
-    var itemData = { "custpage_parent": "30897", "custpage_tranid": "3478837", "custpage_itemtype": "2" }
-    
-    itemHelper.createItem(itemData)
-  
+  var itemData = {
+    custpage_parent: '30897',
+    custpage_tranid: '3478837',
+    custpage_itemtype: '2'
+  }
+
+  itemHelper.createItem(itemData)
+
   // enter functions here
   //
   //
 })
-
-
-
 
 // NG-1682
 require([
@@ -42,34 +182,38 @@ require([
   '/SuiteScripts/WMS/shared/SavedSearchLibrary',
   'SuiteScripts/LIB_SearchHelpers'
 ], function (search, record, ssLib, searchHelpers) {
-  
-    var fittingTypeId = 14
+  var fittingTypeId = 14
 
-var fittingTypeData = search.lookupFields({
-			type: 'customrecord_fittingtype',
-			id: fittingTypeId,
-			columns: ['custrecord_fittingtype_service_item', 'custrecord_fittingcost', 'name']
-		});
+  var fittingTypeData = search.lookupFields({
+    type: 'customrecord_fittingtype',
+    id: fittingTypeId,
+    columns: [
+      'custrecord_fittingtype_service_item',
+      'custrecord_fittingcost',
+      'name'
+    ]
+  })
 
-		var serviceItemInternalId =  fittingTypeData['custrecord_fittingtype_service_item'][0].value
+  var serviceItemInternalId =
+    fittingTypeData['custrecord_fittingtype_service_item'][0].value
 
-		var serviceItemData = search.lookupFields({
-			type: search.Type.SERVICE_ITEM,
-			id: serviceItemInternalId,
-			columns: ['salesdescription', 'custitem_base_price_copy']
-		});
+  var serviceItemData = search.lookupFields({
+    type: search.Type.SERVICE_ITEM,
+    id: serviceItemInternalId,
+    columns: ['salesdescription', 'custitem_base_price_copy']
+  })
 
-	
-		serviceItemObj = {
-			fittingCost: fittingTypeData['custrecord_fittingcost'],
-			fittingTypeName: fittingTypeData['name'],
-			serviceItemId: fittingTypeData['custrecord_fittingtype_service_item'][0].text,
-			serviceItemInternalId: serviceItemInternalId,
-			serviceItemDescription: serviceItemData['salesdescription'],
-			serviceItemPrice: serviceItemData['custitem_base_price_copy'],
-		}
+  serviceItemObj = {
+    fittingCost: fittingTypeData['custrecord_fittingcost'],
+    fittingTypeName: fittingTypeData['name'],
+    serviceItemId:
+      fittingTypeData['custrecord_fittingtype_service_item'][0].text,
+    serviceItemInternalId: serviceItemInternalId,
+    serviceItemDescription: serviceItemData['salesdescription'],
+    serviceItemPrice: serviceItemData['custitem_base_price_copy']
+  }
 
-		return serviceItemObj
+  return serviceItemObj
 })
 
 // NG-1757

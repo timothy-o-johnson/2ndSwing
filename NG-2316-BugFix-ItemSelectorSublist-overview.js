ef4aa545e631c2f9ae24da58b52f21d354a37428
Timothy Johnson <tjohnson@2ndswing.com>
2020-10-30 11:45:43 -0400

    periodic save

---
+++ NG-2316-BugFix-ItemSelectorSublist-overview.js
@@ -0,0 +1,4 @@
+var ng2316 = {
+  url:
+    'https://4537321-sb1.app.netsuite.com/app/site/hosting/scriptlet.nl?script=44&deploy=1&compid=4537321_SB1&custpage_source=opportunity&custpage_trantype=purchaseorder&custpage_tranid=10503026&custpage_itemtype=3&custpage_category=10&custpage_parent=52787&custpage_cachekey=purchaseorder:10503026:inventoryitem:52787:31067742'
+}

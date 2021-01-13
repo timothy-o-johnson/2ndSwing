44e6fbae12bcc09ebe06a548740b6c7095e7c2dc
Tim Johnson <tjohnson@2ndswing.com>
2021-01-13 14:39:01 -0500

    periodic update

---
+++ JS-scratch.js
@@ -0,0 +1,19 @@
+var scratch = [
+  [
+    {
+      sublistId: 'item',
+      fieldId: 'custcol_wms_recvnotes',
+      line: 'line',
+      value: 'notes'
+    }
+  ],
+  [{ sublistId: 'item', fieldId: 'isclosed', line: 'line', value: true }],
+  [
+    {
+      sublistId: 'item',
+      fieldId: 'custcol_wms_hideinwms',
+      line: 'line',
+      value: true
+    }
+  ]
+]

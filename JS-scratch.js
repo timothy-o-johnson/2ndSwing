c6c133a7059449ab9471c28b3f7772369b6746c2
Tim Johnson <tjohnson@2ndswing.com>
2021-01-20 09:34:01 -0500

    periodic update

--- JS-scratch.js
+++ JS-scratch.js
@@ -1,19 +1,19 @@
-var scratch = [
-  [
-    {
-      sublistId: 'item',
-      fieldId: 'custcol_wms_recvnotes',
-      line: 'line',
-      value: 'notes'
-    }
-  ],
-  [{ sublistId: 'item', fieldId: 'isclosed', line: 'line', value: true }],
-  [
-    {
-      sublistId: 'item',
-      fieldId: 'custcol_wms_hideinwms',
-      line: 'line',
-      value: true
-    }
-  ]
-]
+var body = {
+  itemId: 9554591,
+  itemType: '2',
+  category: '24',
+  freeNotes: 'Did not receive item.',
+  sku: 'AHEAD W OUTER : D-D209554591',
+  urlParams: {
+    script: '586',
+    deploy: '2',
+    compid: '4537321_SB1',
+    cacheKey: 'purchaseorder:10618879:inventoryitem:9554591:31239370',
+    isReceivingNotesObj: 'true',
+    isItemSelector: 'true',
+    itemid: '9554591',
+    originalReferrer:
+      'https://4537321-sb1.app.netsuite.com/app/site/hosting/scriptlet.nl?script=48',
+    custpage_rf_orderid: '10618879'
+  }
+}

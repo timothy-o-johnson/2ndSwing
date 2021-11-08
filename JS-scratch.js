79468965adaf41a9fc7310872930f5a2f4823841
Timothy Johnson <tjohnson@2ndswing.com>
2021-11-08 09:19:58 -0500

    periodic backup

--- JS-scratch.js
+++ JS-scratch.js
@@ -1,78 +1,26 @@
-parentSublistObj = {
+var sublistObj = {
   form: {
-    fieldGroups: {
-      parentfilters: {
-        label: 'Parent Item Filters'
-      }
-    },
-    fields: {
-      custpage_brand: {
-        container: 'parentfilters',
-        id: 'custpage_brand',
-        label: 'Brand',
-        source: 'customrecord_g2_brand',
-        type: 'SELECT',
-        defaultValue: null
-      },
-      custpage_category: {
-        container: 'parentfilters',
-        id: 'custpage_category',
-        label: 'Category',
-        source: 'customrecord_g2_category',
-        type: 'SELECT',
-        defaultValue: null
-      },
-      custpage_parent_item: {
-        container: 'parentfilters',
-        id: 'custpage_parent_item',
-        label: 'Parent Item',
-        source: 'inventoryitem',
-        type: 'SELECT',
-        defaultValue: 'parentItem'
-      }
-    },
-    hideNavBar: true,
-    sublists: {
-      custpage_parentlist: {
-        fields: {
-          custpage_thisone: {
-            id: 'custpage_thisone',
-            label: '&nbsp;',
-            type: 'RADIO'
-          },
-          custpage_parentid: {
-            id: 'custpage_parentid',
-            label: '&nbsp;',
-            type: 'text'
-          }
-        },
-        label: 'Select Parent Item',
-        type: 'list'
-      }
-    },
     title: 'Create Item',
     clientScriptModulePath: '/SuiteScripts/WMS/shared/ItemSelector-Client.js'
   },
   parentSublist: {
-    fields: {
-      custpage_thisone: {
-        id: 'custpage_thisone',
-        label: '&nbsp;',
-        type: 'RADIO'
-      },
-      custpage_parentid: {
-        id: 'custpage_parentid',
-        label: '&nbsp;',
-        type: 'text'
-      }
-    },
     label: 'Select Parent Item',
-    type: 'list'
+    lineCount: 107,
+    displayType: 'NORMAL',
+    helpText: null
   },
   parentIdField: {
     id: 'custpage_parentid',
-    label: '&nbsp;',
-    type: 'text'
-  },
-  parentSublistFields: ['parent1', 'parent2', 'parent3']
+    type: 'text',
+    maxLength: 300,
+    isMandatory: false,
+    alias: 'custpage_parentid',
+    defaultValue: null,
+    richTextHeight: 0,
+    richTextWidth: 0,
+    label: ' ',
+    padding: 0
+  }
 }
+
+sublistObj.parentSublist.lineCount
